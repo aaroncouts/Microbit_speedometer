7db6b5c6dd2e33ac88174fa65970417f318bc6d3
aaroncouts <aaroncouts@gmail.com>
2021-01-02 01:02:10 -0500

    Added comments to JS.

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
+// This program requires a microbit wired with the following:
+//  1. An external button that, when pressed, routes VCC to pin 0.
+//  2. A distance sensor with trigger on pin 1 and echo on pin 2.
+
 input.onButtonPressed(Button.A, function () {
+    // Show kilometers/hour
     basic.showNumber(kph)
     basic.showString("kph")
 })
 input.onButtonPressed(Button.B, function () {
+    // Show miles/hour again
     basic.showNumber(mph)
     basic.showString("mph")
 })
@@ -14,9 +20,11 @@ let kph = 0
 let button_released = 1
 basic.forever(function () {
     if (button_released == 0 && pins.digitalReadPin(DigitalPin.P0) == 0) {
+        // Button must be released before taking another reading
         button_released = 1
     }
     if (button_released == 1 && pins.digitalReadPin(DigitalPin.P0) == 1) {
+        // Main button pushed.  Take two distance readings.
         distance1 = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
@@ -28,9 +36,11 @@ basic.forever(function () {
         DigitalPin.P2,
         PingUnit.Centimeters
         )
+        // Using absolute value here so it will work with subject approaching or leaving
         mps = Math.abs((distance2 - distance1) / 100 * 2)
         // basic.showString("mps:")
         // basic.showNumber(mps)
+        // Calculate speed and display it.
         mph = mps * 2.237
         kph = mps * 3.6
         basic.showNumber(mph)
