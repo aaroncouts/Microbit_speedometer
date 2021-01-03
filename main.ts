86a9922921fb8d6391b2513f8f70748a317b4113
aaroncouts <aaroncouts@gmail.com>
2021-01-03 09:02:10 -0500

    Increased text scroll speed.

--- main.ts
+++ main.ts
@@ -5,23 +5,22 @@
  * 
  * 2. A distance sensor with trigger on pin 1 and echo on pin 2.
  */
-
-// Speed variables
-let kph = 0
-let mph = 0
-let mps = 0
-let distance2 = 0
-let distance1 = 0
-let button_released = 1
-
-// Blink variables
-let high = 30
-let low = 0
-let interval = 150
+/**
+ * Speed variables
+ */
 let brightness = 0
 let prevMillis = 0
 let currentMillis = 0
-
+let mps = 0
+let distance2 = 0
+let distance1 = 0
+let mph = 0
+let kph = 0
+let button_released = 1
+let textSpeed = 150
+// Blink variables
+let high = 30
+let interval = 150
 basic.forever(function () {
     if (button_released == 0 && pins.digitalReadPin(DigitalPin.P0) == 0) {
         // Button must be released before taking another reading
@@ -30,7 +29,7 @@ basic.forever(function () {
     if (button_released == 1 && pins.digitalReadPin(DigitalPin.P0) == 1) {
         // Main button pushed.
         button_released = 0
-        //  Take two distance readings.
+        // Take two distance readings.
         distance1 = sonar.ping(
         DigitalPin.P1,
         DigitalPin.P2,
@@ -47,23 +46,22 @@ basic.forever(function () {
         // Calculate speed and display it.
         mph = Math.round(mps * 22.37) / 10
         kph = Math.round(mps * 36) / 10
-        basic.showString("" + convertToText(mph) + "mph")
+        basic.showString("" + convertToText(mph) + "mph", textSpeed)
     }
     if (input.buttonIsPressed(Button.A)) {
         basic.clearScreen()
-        // Button A shows kilometers per hour
-        basic.showString("" + convertToText(kph) + "kph")
+        basic.showString("" + convertToText(kph) + "kph", textSpeed)
     }
     if (input.buttonIsPressed(Button.B)) {
         // Button B shows miles per hour again
         basic.clearScreen()
-        basic.showString("" + convertToText(mph) + "mph")
+        basic.showString("" + convertToText(mph) + "mph", textSpeed)
     }
-
     // Blink. Just to show that the device is on,
     // so we don't run down the battery.
     currentMillis = control.millis()
     if (currentMillis - prevMillis > interval) {
+        let low = 0
         prevMillis = currentMillis
         if (brightness == low) {
             brightness = high
